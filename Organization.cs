c75775669bb6d141d44039a7324cef2c4f466a47
D'Angelo <dpalmieri@anamosa.k12.ia.us>
2026-03-25 10:24:58 -0500

    Update Organization.cs

--- Organization.cs
+++ Organization.cs
@@ -1,11 +1,9 @@
 ﻿namespace knightmoves;
 
 public class Organization{
-   private string Name;
+   private string Name { get; };
 
    public Organization(string name){
       Name = name;
-      public string Person { get => name }
-      return Person;
    }
 }
